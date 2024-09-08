1696f6ce391948e0f8f1148b03bb3f1b4422cd0e
frijoles4me <36173111+frijoles4me@users.noreply.github.com>
2024-09-08 03:22:43 -0700

    Add files via upload

--- pages.js
+++ pages.js
@@ -1,5 +1,6 @@
 function displayText() {
     var text = document.getElementById("textField");
-    text.style.display = "block";
+    // text.style.display = "block"; add to div[style="display:none;"] to work
+    text.innerHTML="<div>this in innerHTML text</div>"
 
 }
