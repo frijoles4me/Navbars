2111fd5d706aa8a36293d29ff3f54a7cb0f6e816
frijoles4me <36173111+frijoles4me@users.noreply.github.com>
2024-09-09 16:15:05 -0700

    Add files via upload

--- pages.js
+++ pages.js
@@ -1,6 +1,25 @@
-function displayText() {
+function displayHome() {
     var text = document.getElementById("textField");
     // text.style.display = "block"; add to div[style="display:none;"] to work
-    text.innerHTML="<div>this in innerHTML text</div>"
+    text.innerHTML="<div class='container text-center'><div class='row'><div class='col col1'>Column</div><div class='col col2'>Column</div><div class='col col3'>Column</div></div></div>";
+}
+function displayService() {
+    var text = document.getElementById("textField");
+    // text.style.display = "block"; add to div[style="display:none;"] to work
+    text.innerHTML="<div>this in innerHTML Service Page</div>"
+
+}
+
+function displayContacts() {
+    var text = document.getElementById("textField");
+    // text.style.display = "block"; add to div[style="display:none;"] to work
+    text.innerHTML="<div>this in innerHTML Contacts Page</div>"
+
+}
+
+function displayAbout() {
+    var text = document.getElementById("textField");
+    // text.style.display = "block"; add to div[style="display:none;"] to work
+    text.innerHTML="<div>this in innerHTML About Page</div>"
 
 }
